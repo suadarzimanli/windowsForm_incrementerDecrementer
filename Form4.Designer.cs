2297c6ce13d9d786708b9fea3eb05e071990e5cd
Suad Arzimanli <97065913+suadarzimanli@users.noreply.github.com>
2023-03-01 23:52:18 +0100

    Add files via upload

---
+++ Form4.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WFA
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.btnClick2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(186, 247);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(183, 64);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "increase";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // btnClick2
+            // 
+            this.btnClick2.Location = new System.Drawing.Point(753, 247);
+            this.btnClick2.Name = "btnClick2";
+            this.btnClick2.Size = new System.Drawing.Size(183, 64);
+            this.btnClick2.TabIndex = 1;
+            this.btnClick2.Text = "decrease";
+            this.btnClick2.UseVisualStyleBackColor = true;
+            this.btnClick2.Click += new System.EventHandler(this.btnClick2_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.btnClick2);
+            this.Controls.Add(this.btnClick);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Button btnClick2;
+    }
+}
